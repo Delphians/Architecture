6c137c95a908b24ef03dc19d6e561118e2294640
Delphians <igor.v.chebotarev@gmail.com>
2024-07-01 12:13:50 +0200

    ChartIntention immutability

--- features/chart/src/main/kotlin/com/che/architecture/features/chart/mvi/ChartIntention.kt
+++ features/chart/src/main/kotlin/com/che/architecture/features/chart/mvi/ChartIntention.kt
@@ -1,7 +1,9 @@
 package com.che.architecture.features.chart.mvi
 
+import androidx.compose.runtime.Immutable
 import kotlinx.collections.immutable.PersistentList
 
+@Immutable
 internal sealed interface ChartIntention {
     data class InitialIntention(
         val points: PersistentList<Double>
