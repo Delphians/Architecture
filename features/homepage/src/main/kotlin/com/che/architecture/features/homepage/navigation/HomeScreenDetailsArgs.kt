eaf82fb767f4e5b46438655d0552ee8a316e4197
Delphians <igor.v.chebotarev@gmail.com>
2024-07-01 13:50:29 +0200

    HomeScreen stability updates

--- features/homepage/src/main/kotlin/com/che/architecture/features/homepage/navigation/HomeScreenDetailsArgs.kt
+++ features/homepage/src/main/kotlin/com/che/architecture/features/homepage/navigation/HomeScreenDetailsArgs.kt
@@ -1,8 +1,10 @@
 package com.che.architecture.features.homepage.navigation
 
+import androidx.compose.runtime.Immutable
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
 
+@Immutable
 internal data class HomeScreenDetailsArgs(
     val ticker: String = "ticker"
 ) {
