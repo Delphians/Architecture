1d1efb46240ba0e737eddae823831a528080c8e8
Delphians <igor.v.chebotarev@gmail.com>
2024-07-01 15:45:18 +0200

    PaymentsScreen stability updates

--- features/payments/src/main/kotlin/com/che/architecture/features/payments/mvi/PaymentsIntention.kt
+++ features/payments/src/main/kotlin/com/che/architecture/features/payments/mvi/PaymentsIntention.kt
@@ -1,14 +1,17 @@
 package com.che.architecture.features.payments.mvi
 
+import androidx.compose.runtime.Immutable
 import com.che.architecture.domain.model.Ticker
 import java.time.LocalDate
 
 internal sealed interface PaymentsIntention {
+    @Immutable
     data class GetTickerPriceIntention(
         val ticker: Ticker,
         val dateRange: ClosedRange<LocalDate>
     ) : PaymentsIntention
 
+    @Immutable
     data class FailureIntention(
         val failure: Throwable
     ) : PaymentsIntention
