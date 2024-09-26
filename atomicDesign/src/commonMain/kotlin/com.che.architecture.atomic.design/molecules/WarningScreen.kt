1bb5bb0fae7ca3c40d40821d67a72ea43b65730e
Delphians <igor.v.chebotarev@gmail.com>
2024-09-26 15:29:08 +0300

    Dev-15 Multiplatform atomic design module

---
+++ atomicDesign/src/commonMain/kotlin/com.che.architecture.atomic.design/molecules/WarningScreen.kt
@@ -0,0 +1,67 @@
+package com.che.architecture.atomic.design.molecules
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
+import com.che.architecture.atomic.design.foundation.ArchitectureTheme
+import com.che.architecture.atomic.design.foundation.dimension.LocalPadding
+import com.che.architecture.atomic.design.resources.ic_warning
+import com.che.architecture.atomic.design.resources.Res
+import com.che.architecture.atomic.design.resources.warning
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
+
+@Composable
+fun WarningScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = vectorResource(resource = Res.drawable.ic_warning),
+            contentDescription = stringResource(resource = Res.string.warning)
+        )
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append("No data")
+                }
+            },
+            modifier = modifier.padding(
+                LocalPadding.current.l
+            )
+        )
+    }
+}
+
+@Composable
+private fun WarningScreenPreview() {
+    ArchitectureTheme {
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            WarningScreen()
+        }
+    }
+}
